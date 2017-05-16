24619c374cf5c379b3310b931a152a0ea397597b
euthanasiolog <piatryxa@gmail.com>
2017-05-16 22:29:30 +0300

    Initial commit

---
+++ src/main/piatr/java/entity/PatientEntity.java
@@ -0,0 +1,9 @@
+package entity;
+
+/**
+ * Created by piatr on 16.05.17.
+ * тут все пациенты больницы
+ * и у каждого есть свой подчиненный список госпитализаций
+ */
+public class PatientEntity {
+}

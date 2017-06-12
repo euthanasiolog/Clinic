3c51bd1a07b5e103dd747682775921106184d834
euthanasiolog <piatryxa@gmail.com>
2017-06-12 22:32:50 +0300

    пишу логику понемногу - дальше в лес

--- src/main/java/piatr/asylum/service/peopleService/patient/PatientService.java
+++ src/main/java/piatr/asylum/service/peopleService/patient/PatientService.java
@@ -1,10 +1,17 @@
 package piatr.asylum.service.peopleService.patient;
 
+import piatr.asylum.entity.clinicEntity.ClinicDepartmentEntity;
+import piatr.asylum.entity.hospitalizationEntity.HospitalizationEntity;
 import piatr.asylum.entity.peopleEntity.PatientEntity;
 import piatr.asylum.service.GenericService;
+
+import java.time.LocalDateTime;
 
 /**
  * Created by piatr on 26.05.17.
  */
 public interface PatientService extends GenericService<PatientEntity>{
+    public void hospitalizationStart(PatientEntity patient, LocalDateTime startTime, ClinicDepartmentEntity clinicDepartment);
+    public void hospitalizationEnd(PatientEntity patient, LocalDateTime localDateTime);
+    public HospitalizationEntity getCurrentHospitalization(PatientEntity patient);
 }
--- src/main/java/piatr/asylum/service/peopleService/patient/impl/PatientServiceImpl.java
+++ src/main/java/piatr/asylum/service/peopleService/patient/impl/PatientServiceImpl.java
@@ -4,9 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import piatr.asylum.dao.GenericDAO;
+import piatr.asylum.dao.peopleDAO.patient.impl.PatientDAOimpl;
+import piatr.asylum.entity.clinicEntity.ClinicDepartmentEntity;
+import piatr.asylum.entity.hospitalizationEntity.HospitalizationEntity;
 import piatr.asylum.entity.peopleEntity.PatientEntity;
 import piatr.asylum.service.GenericServiceImpl;
 import piatr.asylum.service.peopleService.patient.PatientService;
+
+import java.time.LocalDateTime;
 
 /**
  * Created by piatr on 26.05.17.
@@ -14,9 +19,27 @@ import piatr.asylum.service.peopleService.patient.PatientService;
 @Service("patientService")
 @Transactional
 public class PatientServiceImpl extends GenericServiceImpl<PatientEntity> implements PatientService {
+    @Autowired
+    PatientDAOimpl patientDAO;
+
     @Override
     @Autowired
     protected void setGenericDAO(GenericDAO<PatientEntity> genericDAO){
         super.setGenericDAO(genericDAO);
+    }
+
+    @Override
+    public void hospitalizationStart(PatientEntity patient, LocalDateTime startTime, ClinicDepartmentEntity clinicDepartment) {
+        patientDAO.hospitalizationStart(patient, startTime, clinicDepartment);
+    }
+
+    @Override
+    public void hospitalizationEnd(PatientEntity patient, LocalDateTime localDateTime) {
+        patientDAO.hospitalizationEnd(patient, localDateTime);
+    }
+
+    @Override
+    public HospitalizationEntity getCurrentHospitalization(PatientEntity patient) {
+        return patientDAO.getCurrentHospitalization(patient);
     }
 }
